71425bd529d072e7f090ca40e54dc28b7b133709
Olivia-Sastro <im.lyxng@gmail.com>
2025-09-08 15:11:30 +0700

    `Added API key to hello.js handler`

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,4 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Halo Falikh & Ghina 🎉 API kalian jalan di Vercel!" });
+  const key = process.env.API_KEY;cb8f1dda03a4b9caa52e6d660e30b7fcf4de7f26224db2faf6
+  res.status(200).json({ msg: "API jalan", key });
 }
